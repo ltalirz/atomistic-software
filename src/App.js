75cf57b9aaf2aa931ad37bc6d6742d217fd64fbb
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-03 21:37:12 +0100

    table displays

--- src/App.js
+++ src/App.js
@@ -1,15 +1,107 @@
-import './App.css';
+//import './App.css';
+import React from 'react';
+import Table, { Column, SortOrder} from 'react-base-table'
+import 'react-base-table/styles.css'
 
-import { Chart } from './components'
+//import { Chart } from './components'
+//import { Table } from './components'
+import citations from './data/citations.json'
+import codes_metadata from './data/codes_metadata.json'
+import columns from './data/columns.json'
 
-function App() {
-  return (
+// for (let i = 0; i < 3; i++) columns[i].sortable = true
+
+const tableHeight = 1200;
+const rowHeight = 25;
+const colWidth = 100;
+
+// const defaultSort = { key: 'column-0', order: SortOrder.ASC }
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //sortBy: defaultSort,
+      year: "2015",
+      metadata: codes_metadata,
+      citations: citations,
+      sortBy: {key: 'citations', order: SortOrder.DESC },
+    };
+  }
+
+  // onColumnSort = sortBy => {
+  //   this.setState({
+  //     sortBy,
+  //     data: this.state.data.reverse(),
+  //   })
+  // }
+
+  handleChange(event) {
+    this.setState({year: event.target.value});
+  }
+
+  getColumns() {
+    // let columns = [];
+    // for (const key in this.state.metadata['ABINIT']){
+    //   columns.push({ key: key, dataKey: key, width: colWidth});
+    // }
+    // //console.log(columns);
+    // console.log(columns[0].width);
+    // return columns
+    let cols = columns;
+    for (const col of cols) {
+      col['width'] = colWidth;
+      col['dataKey'] = col['key'];
+      col['height'] = 100;
+    }
+    return cols;
+  }
+
+  getData() {
+    let year  = this.state.year.toString();
+    let data = this.state.metadata; //.slice();
+
+    for (const codename in data){
+      data[codename]['citations'] = this.state.citations[year]['citations'][codename];
+    }
+
+    let dataArray = [];
+    for (const codename in data){
+      dataArray.push(data[codename]);
+    }
+    console.log(dataArray[0]);
+    return dataArray;
+    //.return generateData(this.getColumns(),100);
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <Chart />
+      <select value={this.state.year} onChange={this.handleChange}>
+        <option value="2015">2015</option>
+      </select>
+        <Table
+        width={800}
+        height={tableHeight}
+        //fixed
+        rowKey="name"
+        columns={this.getColumns()}
+        data={this.getData()}
+        sortBy={this.state.sortBy}
+        onColumnSort={this.onColumnSort}
+      >
+        </Table>
       </header>
     </div>
   );
+          //width={100}
+        //
+        
+        //<Chart />
+}
 }
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Chart } from './Chart/chart'
+export { default as Table } from './Table/table'
