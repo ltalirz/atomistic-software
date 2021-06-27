2c3a574d648450295c762f89439d28e4929b6131
Leopold Talirz <leopold.talirz@gmail.com>
2021-06-27 14:17:27 +0200

    fix graph layout
     * remove legends for single-code graphs
     * increase legend font size
     * switch to int representation for citation count

--- src/components/Chart/single.js
+++ src/components/Chart/single.js
@@ -4,6 +4,17 @@ import { ResponsiveLine } from "@nivo/line";
 import { getCodeCitations } from "../Config";
 import { useParams } from "react-router-dom";
 
+const THEME = {
+  axis: {
+    legend:{
+      text: {
+        fontSize: '14px',
+      }
+    }
+    
+  }
+};
+
 function getTitle(codeName, citations) {
   /**
    * Return title for chart.
@@ -47,13 +58,44 @@ function SingleChart() {
   let codeName = decodeURIComponent(useParams()["code"]);
   const citations = getCodeCitations([codeName]);
   const title = getTitle(codeName, citations);
-  return nivoChart([{ id: codeName, data: citations }], title);
+  return nivoChart([{ id: codeName, data: citations }], title, false);
 }
 
-function nivoChart(data, title) {
+function nivoChart(data, title, legend=true) {
   /**
    * Return nivo line-chart with default formatting for given data and title.
    */
+  let legend_list = [];
+  let margin_right = 50;
+  if (legend){
+    legend_list=[
+      {
+        anchor: "bottom-right",
+        direction: "column",
+        justify: false,
+        translateX: 100,
+        translateY: 0,
+        itemsSpacing: 0,
+        itemDirection: "left-to-right",
+        itemWidth: 80,
+        itemHeight: 20,
+        itemOpacity: 0.75,
+        symbolSize: 12,
+        symbolShape: "circle",
+        symbolBorderColor: "rgba(0, 0, 0, .5)",
+        effects: [
+          {
+            on: "hover",
+            style: {
+              itemBackground: "rgba(0, 0, 0, .03)",
+              itemOpacity: 1,
+            },
+          },
+        ],
+      },
+    ];
+    margin_right = 130;
+  }
 
   return (
     <div className="container">
@@ -62,7 +104,7 @@ function nivoChart(data, title) {
         <ResponsiveLine
           title={title}
           data={data}
-          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          margin={{ top: 50, right: margin_right, bottom: 50, left: 70 }}
           xScale={{ type: "point" }}
           yScale={{
             type: "linear",
@@ -71,25 +113,18 @@ function nivoChart(data, title) {
             stacked: false,
             reverse: false,
           }}
-          yFormat=" >-.2f"
           axisTop={null}
           axisRight={null}
           axisBottom={{
             orient: "bottom",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
             legend: "Year",
             legendOffset: 36,
             legendPosition: "middle",
           }}
           axisLeft={{
             orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Citations (Google Scholar)",
-            legendOffset: -50,
+            legend: "Citations per year (Google Scholar)",
+            legendOffset: -55,
             legendPosition: "middle",
           }}
           pointSize={10}
@@ -97,35 +132,11 @@ function nivoChart(data, title) {
           pointBorderWidth={2}
           pointBorderColor={{ from: "serieColor" }}
           pointLabelYOffset={-12}
-          enableCrosshair={false}
+          enableCrosshair={true}
           useMesh={true}
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: "left-to-right",
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: "circle",
-              symbolBorderColor: "rgba(0, 0, 0, .5)",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemBackground: "rgba(0, 0, 0, .03)",
-                    itemOpacity: 1,
-                  },
-                },
-              ],
-            },
-          ]}
+          legends={legend_list}
           animate={false}
+          theme={THEME}
         />
       </div>
     </div>
