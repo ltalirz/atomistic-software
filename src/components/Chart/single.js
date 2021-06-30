7d7e1881e103c3898c049f11510c65e60be30b36
Leopold Talirz <leopold.talirz@gmail.com>
2021-06-30 15:04:16 +0200

    fix linter issue

--- src/components/Chart/single.js
+++ src/components/Chart/single.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Title from "../Dashboard/Title";
 import useStyles from "../Dashboard/Styles";
